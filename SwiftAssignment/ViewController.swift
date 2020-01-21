be48091b8d1ff98c27d6ede4be763bab5fa727dc
Dipendra <dipendradubey@gmail.com>
2020-01-21 14:11:22 +0530

    Initial Commit

---
+++ SwiftAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftAssignment
+//
+//  Created by Dipendra Dubey on 21/01/20.
+//  Copyright © 2020 Dipendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
