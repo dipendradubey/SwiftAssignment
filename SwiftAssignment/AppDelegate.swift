36a9d2eca03cdf43a5d3a8bbd4f65de00b3c4c17
Dipendra <dipendradubey@gmail.com>
2020-01-21 14:36:01 +0530

    Mainstoryboard is removed Country Tableview created

--- SwiftAssignment/AppDelegate.swift
+++ SwiftAssignment/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let frame = UIScreen.main.bounds
+        window = UIWindow(frame: frame)
+        let navigationController = UINavigationController(rootViewController: CountryTableViewController())
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
--- SwiftAssignment/CountryTableViewController.swift
+++ SwiftAssignment/CountryTableViewController.swift
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CountryTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Author"
     }
-
-
 }
 
